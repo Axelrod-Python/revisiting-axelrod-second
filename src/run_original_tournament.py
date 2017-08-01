90f0a60751be370e7b08be27978f006d8edc43ca
Vince Knight <vincent.knight@gmail.com>
2017-08-01 17:18:28 +0100

    Write script to reproduce original tournaments.
    There are two scripts here, these correspond to two separate ones with a
    a particular strategy for which we have some ambiguity:
    https://github.com/Axelrod-Python/axelrod-fortran/issues/36

---
+++ src/run_original_tournament.py
@@ -0,0 +1,30 @@
+import sys
+
+import axelrod as axl
+import axelrod_fortran as axlf
+
+import main
+assert axl.__version__ == "3.3.0"
+assert axlf.__version__ == "0.2.0"
+
+players = [axlf.Player(name) for name in axlf.second_tournament_strategies]
+assert len(players) == 63
+
+turns = [63, 77, 151, 308, 157]
+match_attributes={"length": float('inf')}
+
+if __name__ == "__main__":
+
+    seed = int(sys.argv[1])
+    repetitions = int(sys.argv[2])
+
+    for turn in turns:
+        main.main(players=players,
+                  repetitions=repetitions,
+                  seed=seed,
+                  outdir="./data/original_tournament",
+                  prefix="original_{}_turns_{}_repetitions".format(
+                      turn,
+                      repetitions),
+                  turns=turn,
+                  match_attributes=match_attributes)
