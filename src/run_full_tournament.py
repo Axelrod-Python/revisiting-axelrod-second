18b99ffd6eec5cf055a6e04b1db3654a23dabd8e
Vince Knight <vincent.knight@gmail.com>
2017-08-04 13:54:02 +0100

    Write scripts for full tournament.
    This includes long run time strategies.

---
+++ src/run_full_tournament.py
@@ -0,0 +1,32 @@
+import sys
+
+import axelrod as axl
+import axelrod_fortran as axlf
+
+import main
+assert axl.__version__ == "3.3.0"
+assert axlf.__version__ == "0.3.0"
+
+players = [axlf.Player(name) for name in axlf.second_tournament_strategies]
+players += [s() for s in axl.strategies]
+
+assert len(players) == 263
+
+turns = [63, 77, 151, 308, 157]
+match_attributes={"length": float('inf')}
+
+if __name__ == "__main__":
+
+    seed = int(sys.argv[1])
+    repetitions = int(sys.argv[2])
+
+    for turn in turns:
+        main.main(players=players,
+                  repetitions=repetitions,
+                  seed=seed,
+                  outdir="./data/full_tournament",
+                  prefix="full_{}_turns_{}_repetitions".format(
+                      turn,
+                      repetitions),
+                  turns=turn,
+                  match_attributes=match_attributes)
