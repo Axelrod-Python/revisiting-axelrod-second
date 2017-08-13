6407640715e4ff48e9308613c152a492b6937a7b
Vince Knight <vincent.knight@gmail.com>
2017-08-13 19:31:53 +0100

    Add training against full set of strategies.

---
+++ src/data/training_strategies/train_with_all_axelrod_strategies.py
@@ -0,0 +1,28 @@
+"""
+A script to train FSM players against sets of the Fortran players
+"""
+import csv
+import multiprocessing
+import sys
+
+import axelrod_dojo as dojo
+import axelrod_fortran as axlf
+
+from main import train
+
+assert axlf.__version__ == "0.3.1"
+
+
+if __name__ == "__main__":
+    cpu_count = multiprocessing.cpu_count()
+    opponents = [axlf.Player(name)
+                 for name in axlf.second_tournament_strategies]
+    weights = [1 for _ in opponents]
+    assert len(opponents) == 63
+
+    output_file = "training_data_original_tournament_full_players.csv"
+
+    train(opponents=opponents,
+          weights=weights,
+          filename=output_file,
+          processes=cpu_count)
