980a637429ee0c7841d01f9a11842cd3bfbd9719
Vince Knight <vincent.knight@gmail.com>
2017-08-28 11:31:44 +0100

    Add ability to read in a population.
    Going to use this to train for longer.

--- src/data/training_strategies/main.py
+++ src/data/training_strategies/main.py
@@ -4,6 +4,7 @@ A script to train FSM players against sets of the Fortran players
 import csv
 import multiprocessing
 import sys
+import pathlib
 
 import axelrod as axl
 import axelrod_dojo as dojo
@@ -11,16 +12,27 @@ import axelrod_fortran as axlf
 
 assert axl.__version__ == "3.3.0"
 assert axlf.__version__ == "0.3.1"
-assert dojo.__version__ == "0.0.4"
+assert dojo.__version__ == "0.0.5"
 
-def train(opponents, weights, filename, processes):
-    name = "score"
-    turns = 200
-    match_attributes = {"length": float("inf")}
-    repetitions = 100
-    num_states = 10
-    mutation_rate = 0.1
-    size = 16
+def train(opponents, weights, filename, processes,
+          turns=200, repetitions=100, mutation_rate=0.1, size=16,
+          num_states=10, name="score",
+          match_attributes={"length": float("inf")}):
+
+    # Code to read in any pre training and adjust output file
+    current_dir = pathlib.Path(".")
+
+    try:
+        initial_params = dojo.load_params(dojo.FSMParams,
+                                          filename,
+                                          num=16)
+        initial_population = initial_params
+
+        for params in initial_params:
+            params.mutation_rate = 0.1
+
+    except AttributeError:
+        initial_population = None
 
     objective = dojo.prepare_objective(name=name,
                                        turns=turns,
@@ -28,14 +40,16 @@ def train(opponents, weights, filename, processes):
                                        repetitions=repetitions)
 
     population = dojo.Population(params_class=dojo.FSMParams,
-                                 params_args=(num_states, mutation_rate),
+                                 params_args=(num_states,
+                                              mutation_rate),
                                  size=size,
                                  objective=objective,
                                  output_filename=filename,
                                  opponents=opponents,
                                  weights=weights,
                                  bottleneck=5,
-                                 processes=processes)
+                                 processes=processes,
+                                 population=initial_population)
 
     generations = 10 ** 7
     axl.seed(0)
@@ -54,7 +68,6 @@ if __name__ == "__main__":
             weights.append(float(weight))
 
     output_file = "training_data_" + input_file
-
     train(opponents=opponents,
           weights=weights,
           filename=output_file,
