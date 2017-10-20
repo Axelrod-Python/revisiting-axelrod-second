2bdbb3b786d0ba193f5e6d088c413cc80c47fa3f
Vince Knight <vincent.knight@gmail.com>
2017-10-20 07:52:25 +0100

    Modify tournament script for self interactions.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import axelrod as axl
+import axelrod_fortran as axlf
 import numpy as np
 
 assert axl.__version__ == "3.8.1"
-assert np.__version__ == "1.13.3"
+assert np.__version__ == "1.13.1"
+
+copy_of_shared_library = "libstrategies_copy.so"
 
 def main(players,
          repetitions=1000,
@@ -13,6 +16,9 @@ def main(players,
          **tournament_kwargs):
     """
     A function to run a tournament.
+
+    To ensure self interactions are recorded correctly, this is done using a
+    spatial tournament and using a copied version of the shared library.
 
     This output data to be analysed at a later date which allows for large
     number of repetitions to be computed independently.
@@ -32,8 +38,30 @@ def main(players,
             - match_attributes
 
     """
+    fortran_players = [player for player in players
+                       if type(player) is axlf.Player]
+    player_copies = []
+    for player in players:
+        original_name = player.original_name
+        player_copies.append(axlf.Player(
+            original_name,
+            shared_library_name=copy_of_shared_library))
+
+    number_of_players = len(players)
+    # Normal interactions
+    edges = [(i, j) for i in range(number_of_players)
+             for j in range(number_of_players) if i < j]
+    # Self interactions
+    edges += [(i, i) for i, player in enumerate(players)
+              if player not in fortran_players]
+    # Self interactions for fortran players
+    edges += [(i, i + number_of_players)
+              for i, player in enumerate(players)
+              if player in fortran_players]
+
     axl.seed(seed)
-    tournament = axl.Tournament(players, repetitions=repetitions,
+    tournament = axl.Tournament(players + player_copies,
+                                edges=edges, repetitions=repetitions,
                                 **tournament_kwargs)
     interaction_filename = "{}/{}_{}_interactions.csv".format(outdir,
                                                               prefix,
