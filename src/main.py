361fbd154e71258db815f1ada24657adc8765a7f
Vince Knight <vincent.knight@gmail.com>
2018-01-24 09:47:28 +0000

    Update to latest version of libraries.

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ import axelrod as axl
 import axelrod_fortran as axlf
 import numpy as np
 
-assert axl.__version__ == "3.10.0"
-assert axlf.__version__ == "0.4.1"
+assert axl.__version__ == "3.11.0"
+assert axlf.__version__ == "0.4.2"
 assert np.__version__ == "1.13.1"
 
 copy_of_shared_library = "libstrategies_copy.so"
