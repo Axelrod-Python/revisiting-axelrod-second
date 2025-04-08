a753b739d0449b805ed74c6626dfaf3e340b276d
Vince Knight <vince@vknight.org>
2025-04-08 10:37:20 +0100

    Add script to get python champion.

---
+++ src/run_original_tournament_with_python_champion.py
@@ -0,0 +1,30 @@
+import sys
+
+import axelrod as axl
+import axelrod_fortran as axlf
+
+import main
+
+players = [axlf.Player(name) for name in axlf.second_tournament_strategies 
+           if name != "k61r"]
+players.append(axl.Champion())
+assert len(players) == 63
+
+turns = [63, 77, 151, 308, 156]
+match_attributes={"length": float('inf')}
+
+if __name__ == "__main__":
+
+    seed = int(sys.argv[1])
+    repetitions = int(sys.argv[2])
+
+    for turn in turns:
+        main.main(players=players,
+                  repetitions=repetitions,
+                  seed=seed,
+                  outdir="./data/original_tournament_with_python_champion",
+                  prefix="original_with_py_champion_{}_turns_{}_repetitions".format(
+                      turn,
+                      repetitions),
+                  turns=turn,
+                  match_attributes=match_attributes)
