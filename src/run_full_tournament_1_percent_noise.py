b69672e76896f50a3a915b5281dad4f260c6806c
Vince Knight <vince@vknight.org>
2025-06-09 06:28:25 +0100

    Add noise tournament run (#9)
    * Add more jobs
    
    - Add noise (need to remove commented out jobs in jobs.txt)
    - Analyse tournament with 4 extra strategies (need to replace commented
      out line)
    
    * Add noise scripts.
    
    * Update new assets for noise and N=4 extra invites
    
    * Add first draft of conclusion and more plots
    
    Add a plot showing the change of rank across tournaments.
    
    * Add rank change plots and some other assets.

---
+++ src/run_full_tournament_1_percent_noise.py
@@ -0,0 +1,36 @@
+import sys
+
+import axelrod as axl
+import axelrod_fortran as axlf
+
+import main
+
+players = [axlf.Player(name) for name in axlf.second_tournament_strategies]
+implemented_strategies = [axlf.characteristics[name]['axelrod-python_class']
+                          for name in axlf.second_tournament_strategies]
+players += [s() for s in axl.strategies
+            if s not in implemented_strategies and
+            s not in [axl.Alexei, axl.ContriteTitForTat]]  # Correspond to TfT
+
+assert len(players) == 272, len(players)
+
+turns = [63, 77, 151, 308, 156]
+noise = 0.01
+match_attributes={"length": float('inf')}
+
+if __name__ == "__main__":
+
+    seed = int(sys.argv[1])
+    repetitions = int(sys.argv[2])
+
+    for turn in turns:
+        main.main(players=players,
+                  repetitions=repetitions,
+                  seed=seed,
+                  outdir="./data/full_tournament_1_percent_noise",
+                  prefix="full_{}_turns_{}_repetitions".format(
+                      turn,
+                      repetitions),
+                  turns=turn,
+                  noise=noise,
+                  match_attributes=match_attributes)
