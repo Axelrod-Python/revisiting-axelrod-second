c67846d8324296049b91e1f43932c5352d0810e8
Vince Knight <vince@vknight.org>
2026-04-27 10:13:37 +0100

    Correct error with dir name and missing file.

--- src/run_original_tournament_with_probabilistic_ending.py
+++ src/run_original_tournament_with_probabilistic_ending.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         players=players,
         repetitions=repetitions,
         seed=seed,
-        outdir="./data/original_tournament_probabilistic_end",
+        outdir="./data/original_tournament_with_probabilistic_end",
         prefix="original_{}_prob_end_{}_repetitions".format(prob_end, repetitions),
         prob_end=prob_end,
         match_attributes=match_attributes,
